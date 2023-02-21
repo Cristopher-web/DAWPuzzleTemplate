81522b8d5b9be0db6add484e30439b6638706aef
DAWTarde <DAWTarde@10.2.1.177>
2023-02-21 16:27:20 +0100

    PrimerCommitTempPuzzle

---
+++ src/main/java/com/mycompany/dawpuzzletemplate/Shuttle.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.dawpuzzletemplate;
+
+/**
+ *
+ * @author DAWTarde
+ */
+public class Shuttle {
+   private float angle;
+   private Bubble burbujaActual;
+   private Bubble burbujaSiguiente;
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module pedro.ieslaencanta.com.dawpuzzletemplate {
+    requires javafx.controls;
+    requires javafx.media;
+    exports com.mycompany.dawpuzzletemplate;
+}
